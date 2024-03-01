cfe67139f51ff2d3f53a2d29c2e26ece1517cdad
Petri <peewter@gmail.com>
2024-03-01 16:47:27 +0200

    Import styles and PrimeVue

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css'
+import './assets/styles/global.css'
+
+import 'primevue/resources/themes/aura-light-amber/theme.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
